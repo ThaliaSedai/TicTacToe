5a69981a191d6e4457fcd14f49b3e797e99f93b9
ThaliaSedai <anastasija.zidai@gmail.com>
2025-01-06 15:13:19 +0100

    Code Polished, and button fixed cant be clicked more times then one

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -21,7 +21,7 @@ export default function GameBoard({ onSelectSquare, turns }) {
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                                <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null}>{playerSymbol}</button>
                             </li>
                         ))}
                     </ol>
@@ -30,17 +30,3 @@ export default function GameBoard({ onSelectSquare, turns }) {
         </ol>
     );
 }
-
-
-
-// const [ gameBoard, setGameBoard ] = useState(initialGameBoard);
-
-// function handleSelectSquare(rowIndex, colIndex) {
-//     setGameBoard((prevGameBoard) => {
-//         const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-//         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-//         return updatedBoard;
-//     });
-
-//     onSelectSquare();
-// }
